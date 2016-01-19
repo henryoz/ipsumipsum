2f7f1c2ac0e7e0f0294becf88845d5f1ae15f06d
Henry Osadzinski <henry@osadzinski.co.uk>
2016-01-19 16:58:13 +0000

    adding and retrieving basic entries

--- ipsumipsum.js
+++ ipsumipsum.js
@@ -3,20 +3,26 @@ Ipsums = new Mongo.Collection("ipsums");
 if (Meteor.isClient) {
 
   Template.ipsums.helpers({
-    ipsums: function () {
-      return Ipsums.find({}, );
+    count: function () {
+      return Ipsums.find().count();
+    },
+    ipsums: function() {
+      return Ipsums.find({}, {sort: {'createdAt': -1}});
     }
   });
 
   Template.ipsumForm.events({
     'submit .ipsum': function(e) {
       e.preventDefault();
+      var title = e.target.title.value;
       var text = e.target.text.value;
       
       Ipsums.insert({
+        title: JSON.stringify(title),
         text: JSON.stringify(text),
         createdAt: new Date()
       });
+
 
       e.target.text.value = '';
 
