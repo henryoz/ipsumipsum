47296e85453d67b3c31244c78f48f5ef20df7365
Henry Osadzinski <henry@osadzinski.co.uk>
2016-01-20 12:41:53 +0000

    added momentjs and split text into array

--- ipsumipsum.js
+++ ipsumipsum.js
@@ -16,14 +16,19 @@ if (Meteor.isClient) {
       e.preventDefault();
       var title = e.target.title.value;
       var text = e.target.text.value;
+      var textArray = text.split('\n');
+      var now = new Date();
+      var readableDate = moment().format("HH:mm ddd do MMM");
       
       Ipsums.insert({
-        title: JSON.stringify(title),
-        text: JSON.stringify(text),
-        createdAt: new Date()
+        title: title,
+        text: textArray,
+        moment: readableDate,
+        createdAt: now
       });
 
 
+      e.target.title.value = '';
       e.target.text.value = '';
 
     }
